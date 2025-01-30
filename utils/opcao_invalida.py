42bf44784e34154c8c0cc1572c6e5ed28398e4b0
Harenza <alan.harenzap@gmail.com>
2025-01-30 10:22:55 -0300

    criacao das funcoes em utils

---
+++ utils/opcao_invalida.py
@@ -0,0 +1,11 @@
+import voltar_menu_principal
+
+def opcao_invalida():
+    '''
+    Exibe mensagem de opção inválida e retorna ao menu principa
+
+    Outputs:
+    - Retorna ao menu principal
+    '''
+    print('Opção inválida!\n')
+    voltar_menu_principal()
---
+++ utils/voltar_menu_principal.py
@@ -0,0 +1,8 @@
+from aplicacoes.main import main
+
+def voltar_menu_principal():
+    '''
+    Solicita uma tecla para voltar ao menu principal
+    '''
+    input('\nDigite uma tecla para voltar ao menu principal ')
+    main()
